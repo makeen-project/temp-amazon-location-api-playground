f850aba73fb6eaa0799fd7cf4fd1caafb7c820e4
makeen <touqeer-makeen@xtabbass-MacBook-Pro.local>
2025-08-25 22:43:16 +0500

    improve __info-icon radio box style; and make tooltip show on hover and stay on click

--- src/atomicui/atoms/RadioButton/RadioButton.tsx
+++ src/atomicui/atoms/RadioButton/RadioButton.tsx
@@ -3,9 +3,9 @@
  * SPDX-License-Identifier: MIT-0
  */
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-import { IconClose, IconInfo } from "@api-playground/assets/svgs";
+import { IconClose, IconInfoSolid } from "@api-playground/assets/svgs";
 import { Flex, Radio, RadioGroupField } from "@aws-amplify/ui-react";
 import { Tooltip } from "react-tooltip";
 
@@ -78,9 +78,51 @@ export const RadioButtonGroup = <T extends string>({
 	disabled = false,
 	className = ""
 }: RadioButtonGroupProps<T>): React.ReactElement => {
+	const [clickedTooltips, setClickedTooltips] = useState<Set<string>>(new Set());
+
 	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
 		onChange?.(e.target.value as T);
 	};
+
+	const handleTooltipToggle = (tooltipId: string) => {
+		setClickedTooltips(prev => {
+			const newSet = new Set(prev);
+			if (newSet.has(tooltipId)) {
+				newSet.delete(tooltipId);
+			} else {
+				newSet.add(tooltipId);
+			}
+			return newSet;
+		});
+	};
+
+	// Watch for tooltip removal from DOM and clear local state
+	useEffect(() => {
+		const observer = new MutationObserver((mutations) => {
+			mutations.forEach((mutation) => {
+				mutation.removedNodes.forEach((node) => {
+					if (node.nodeType === Node.ELEMENT_NODE) {
+						const element = node as Element;
+						// Check if removed element is a tooltip
+						if (element.classList.contains('react-tooltip')) {
+							// Clear all tooltips from local state when any tooltip is removed
+							setClickedTooltips(new Set());
+						}
+					}
+				});
+			});
+		});
+
+		// Start observing the document body for DOM changes
+		observer.observe(document.body, {
+			childList: true,
+			subtree: true
+		});
+
+		return () => {
+			observer.disconnect();
+		};
+	}, []);
 
 	if (defaultValue && !options.some(opt => opt.value === defaultValue)) {
 		throw new Error(
@@ -100,31 +142,62 @@ export const RadioButtonGroup = <T extends string>({
 				</Flex>
 			)}
 			<RadioGroupField direction="row" name={name} defaultValue={defaultValue} value={value} onChange={handleChange} legend="">
-				{options.map(option => (
-					<Flex alignItems="center" key={option.value} className="radio-option">
-						<Radio className="custom-radio" value={option.value} isDisabled={disabled || option.disabled}>
-							{option.label}
-						</Radio>
-						{option.tooltipText && (
-							<>
-								<IconInfo data-tooltip-id={`radio-option-${name}-${option.value}`} className="radio-group__info-icon" />
-								<Tooltip
-									classNameArrow="radio-group__arrow-icon"
-									arrowColor="var(--grey-color-2)"
-									openOnClick
-									id={`radio-option-${name}-${option.value}`}
-									className="react-tooltip"
-									place="top"
-								>
-									{option.tooltipText}
-									<span className="radio-group__close-icon-container">
-										<IconClose className="radio-group__close-icon" />
-									</span>
-								</Tooltip>
-							</>
-						)}
-					</Flex>
-				))}
+				{options.map(option => {
+					const tooltipId = `radio-option-${name}-${option.value}`;
+					const isClicked = clickedTooltips.has(tooltipId);
+
+					return (
+						<Flex alignItems="center" key={option.value} className="radio-option">
+							<Radio className="custom-radio" value={option.value} isDisabled={disabled || option.disabled}>
+								{option.label}
+							</Radio>
+							{option.tooltipText && (
+								<>
+									<IconInfoSolid
+										onClick={(e) => {
+											e.stopPropagation();
+											handleTooltipToggle(tooltipId);
+										}}
+										data-tooltip-id={`radio-option-${name}-${option.value}`}
+										className="radio-group__info-icon"
+									/>
+									<Tooltip
+										classNameArrow="radio-group__arrow-icon"
+										arrowColor="var(--grey-color-2)"
+										openOnClick
+										id={`radio-option-${name}-${option.value}`}
+										className="react-tooltip"
+										place="top"
+										openEvents={{ mouseenter: true, focus: true }}
+										closeEvents={{ mouseleave: !isClicked, blur: !isClicked }}
+										globalCloseEvents={{
+											escape: true,
+											scroll: !isClicked,
+											resize: !isClicked,
+											clickOutsideAnchor: true
+										}}
+									>
+										{option.tooltipText}
+										<span
+											onClick={(e) => {
+												e.stopPropagation();
+												// Remove this tooltip from clicked state to close it
+												setClickedTooltips(prev => {
+													const newSet = new Set(prev);
+													newSet.delete(tooltipId);
+													return newSet;
+												});
+											}}
+											className="radio-group__close-icon-container"
+										>
+											<IconClose className="radio-group__close-icon" />
+										</span>
+									</Tooltip>
+								</>
+							)}
+						</Flex>
+					);
+				})}
 			</RadioGroupField>
 		</Flex>
 	);
