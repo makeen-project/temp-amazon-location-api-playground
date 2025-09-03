651df527721937668f812190dd5c74eed20aa5f5
Zeeshan Sheikh <zeeshan@makeen.io>
2025-09-03 19:49:40 +0500

    fixed test cases

--- src/atomicui/molecules/CheckboxGroup/CheckboxGroup.test.tsx
+++ src/atomicui/molecules/CheckboxGroup/CheckboxGroup.test.tsx
@@ -5,7 +5,7 @@
 
 import { faker } from "@faker-js/faker";
 import { RenderResult, act, render, screen } from "@testing-library/react";
-import i18n from "locales/i18n";
+import i18n from "@api-playground/locales/i18n";
 import { I18nextProvider } from "react-i18next";
 
 import CheckboxGroup from ".";
